2e4ecc581d268bc9067fc12142e52b0fe54ea081
Abel Backschadt <abel.backschadt1@gmail.com>
2023-06-22 23:23:07 +0800

    June 22 progress, added css, learned template from figma

--- src/pages/login.jsx
+++ src/pages/login.jsx
@@ -1,8 +1,48 @@
+import "../styles/login.css";
+
 const LoginPage = () => {
-    return (
-        <div>
-            Test
-        </div>
+    return ( 
+         <div className="LoginCSS">
+            <div>
+                test
+            </div>
+            <div className="wrapper">
+                <div className="logo">
+                    <div className="logopicture">
+                        <div className="group1">
+
+                        </div>
+                    </div>
+                </div>
+                
+            <div className="container">
+                <body className="username">
+                    <div className="inputlayout">
+                        <text className="text">
+                            Email
+                        </text>
+                    </div>
+                </body>
+
+                <body className="password">
+                    <div className="inputlayout">
+                        <text className="text">
+                            Password
+                        </text>
+                    </div>
+                </body>
+                <div className="loginButton">
+                    <div className="loginText">
+                        Login
+                    </div>
+                    <div className="forgot">
+                        Forgot Password?
+                    </div>
+                </div>
+                
+            </div>
+        </div> 
+     </div>       
     ) 
 }
 export default LoginPage
