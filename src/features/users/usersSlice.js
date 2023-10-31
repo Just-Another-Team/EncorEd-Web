28f2748b8b5c9139da3dd57c7505a9b10a52c6ff
Andrew Abella <shisunewolf@gmail.com>
2023-10-31 17:46:47 +0800

    Add User function completed

--- src/features/users/usersSlice.js
+++ src/features/users/usersSlice.js
@@ -16,25 +16,48 @@ export const getUsers = createAsyncThunk(
     }
 )
 
+export const addUsers = createAsyncThunk(
+    "/users/add",
+    async (userData, {rejectWithValue}) => {
+        return await EncorEdAuthService.addUser(userData)
+            .then((res) => res)
+            .catch((error) => rejectWithValue(error))
+    }
+)
+
 const usersSlice = createSlice({
     name: 'users',
     initialState,
     extraReducers: (builder) => {
-        builder.addCase(getUsers.pending, (state, action) => {
+        {
+            builder.addCase(addUsers.pending, (state, actions) => {
+                state.loading = true
+            })
+            builder.addCase(addUsers.fulfilled, (state, actions) => {
+                state.loading = false
+            })
+            builder.addCase(addUsers.rejected, (state, actions) => {
+                state.loading = false
+                state.error = actions.payload
+            })
+        }
+        {
+            builder.addCase(getUsers.pending, (state, action) => {
             state.loading = true
             state.users = []
             state.error = null
-        })
-        builder.addCase(getUsers.fulfilled, (state, action) => {
-            state.loading = false
-            state.users = action.payload.data
-            state.error = null
-        })
-        builder.addCase(getUsers.rejected, (state, action) => {
-            state.loading = false
-            state.users = []
-            state.error = action.payload
-        })
+            })
+            builder.addCase(getUsers.fulfilled, (state, action) => {
+                state.loading = false
+                state.users = action.payload.data
+                state.error = null
+            })
+            builder.addCase(getUsers.rejected, (state, action) => {
+                state.loading = false
+                state.users = []
+                state.error = action.payload
+            })
+        }
     }
 })
 
