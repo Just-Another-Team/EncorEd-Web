0f9fc0e1791478f5c1e56f14d9de3d80c7cd5ce8
Gabriel Alpha Aguilar <gabrielalpha98@gmail.com>
2023-11-17 18:35:50 +0800

    Added Subject Schedule Inputs

--- src/pages/Authenticated/Subject/pages/AddSubject.tsx
+++ src/pages/Authenticated/Subject/pages/AddSubject.tsx
@@ -1,11 +1,14 @@
-import React from "react"
+import React, { useState } from "react"
 import {
     Box,
     Button,
     Divider,
     Grid,
+    IconButton,
+    Stack,
     Tab,
     Tabs,
+    TextField,
     Typography,
 } from '@mui/material'
 import { useForm } from 'react-hook-form'
@@ -15,6 +18,9 @@ import { useNavigate } from "react-router-dom"
 import FormInputTextField from "../../../../components/TextField/FormInputTextField"
 import FormInputDropDown from "../../../../components/DropDown/FormInputDropDown"
 import { RegisterFormInput } from "../../../../types/RegisterFormInput"
+import SubjectEventCard from "../../../../components/Cards/SubjectEventCard"
+import ArrowForwardIosOutlinedIcon from '@mui/icons-material/ArrowForwardIosOutlined';
+import CloseOutlinedIcon from '@mui/icons-material/CloseOutlined';
 
 type SubjectInput = {
     name?: string; 
@@ -37,6 +43,8 @@ const AddSubject = () => {
     const user = useAppSelector(state => state.authentication.data.email)
     const institution = useAppSelector(state => state.institution.data.name)
 
+    const [showSchedule, setShowSchedule] = useState(false);
+
     const {handleSubmit, reset, control, setValue, formState: {errors}} = useForm<SubjectInput>({
         defaultValues: {
             name: "", 
@@ -52,6 +60,14 @@ const AddSubject = () => {
         console.log(data)
 
         reset();
+    }
+
+    const openSchedule = (e: FixMeLater) => {
+        setShowSchedule(true)
+    }
+
+    const closeSchedule = (e: FixMeLater) => {
+        setShowSchedule(false)
     }
 
     const subjectInputs: Array<RegisterFormInput> = [
@@ -72,8 +88,8 @@ const AddSubject = () => {
                 ADD SUBJECT
             </Typography>
 
-            <Grid spacing={4} container>
-                <Grid md={6} item>
+            <Grid container>
+                <Grid xs={12} md={6} item>
 
                     <Box marginBottom={2} sx={{ borderBottom: 1, borderColor: 'divider' }}>
                         <Tabs value={0} onChange={() => {}} aria-label="basic tabs example">
@@ -96,31 +112,35 @@ const AddSubject = () => {
                                 </Grid>             
                             ))}
 
-                            {/* <Grid xs={12} lg={6} item>
-                                <Typography variant="body1" marginBottom={1}>Title</Typography>
-                                <FormInputTextField name="Title" control={control} fullWidth/>
-                            </Grid>
-
-                            <Grid xs={12} lg={6} item>
-                                <Typography variant="body1" marginBottom={1}>EDP Code</Typography>
-                                <FormInputTextField name="edpCode" control={control} fullWidth/>
-                            </Grid>
-
-                            <Grid xs={12} lg={6} item>
-                                <Typography variant="body1" marginBottom={1}>Units</Typography>
-                                <FormInputTextField name="units" control={control} fullWidth/>
-                            </Grid>
-
-                            <Grid xs={12} lg={6} item>
-                                <Typography variant="body1" marginBottom={1}>Type</Typography>
-                                <FormInputDropDown name="type" control={control} options={subjectTypes} fullWidth />
-                            </Grid> */}
                         </Grid>
 
-                        <Grid rowSpacing={1} columnSpacing={4} container marginBottom={2}>
+                        
+
+                        <Grid container marginBottom={2} justifyContent={"space-between"}>
+                            {showSchedule ? 
+                            <>
+                                <Grid xs={11} item marginBottom={1}>
+                                    <Typography variant="h6" color={"#296EB4"} fontWeight={700}>Schedule</Typography>
+                                </Grid>
+                                <Grid display={"flex"} xs={1} item justifyContent={"center"} alignItems={"center"} marginBottom={1}>
+                                    <IconButton size="small" onClick={closeSchedule}>
+                                        <CloseOutlinedIcon fontSize="inherit" />
+                                    </IconButton>
+                                </Grid>
+
+                                <Grid xs={5} item>
+                                    <TextField label="Start" fullWidth />
+                                </Grid>
+                                <Grid display={"flex"} xs={1} item justifyContent={"center"} alignItems={"center"}>
+                                    <ArrowForwardIosOutlinedIcon />
+                                </Grid>
+                                <Grid xs={5} item>
+                                    <TextField label="End" fullWidth/>
+                                </Grid>
+                            </> : 
                             <Grid xs={12} item>
-                                <Button variant="outlined">Add Schedule</Button>
-                            </Grid>
+                                <Button onClick={openSchedule} variant="outlined">Add Schedule</Button>
+                            </Grid>}
                         </Grid>
 
                         <Grid rowSpacing={1} columnSpacing={4} container marginBottom={2}>
@@ -135,13 +155,18 @@ const AddSubject = () => {
                             </Grid>
                         </Grid>
                     </Box>
+
                 </Grid>
-                <Grid md={6} item>
-                    <Typography>Added Subjects here</Typography>
+                <Grid position={"relative"} minHeight={480} xs={12} md={6} item padding={2}>
+                    <Stack padding={2} gap={2} position={"absolute"} top={0} right={0} left={0} bottom={0} overflow={'auto'}>
+                        {Array.from({length: 4}).map(el => (
+                            <SubjectEventCard />
+                        ))}
+                    </Stack>
                 </Grid>
             </Grid>
 
-            <Grid justifyContent={"flex-end"} spacing={1} container>
+            <Grid justifyContent={"flex-end"} marginY={1} spacing={1} container>
                 <Grid xs={12} sm={2} lg={1} item>
                     <Button fullWidth variant="contained">Submit</Button>
                 </Grid>
